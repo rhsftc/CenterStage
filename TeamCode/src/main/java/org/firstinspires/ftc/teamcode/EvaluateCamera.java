ce243c250c1e7d17e2950581f254b40d36d02629
rhsftc <ronsearle@hotmail.com>
2023-09-21 12:11:52 -0500

    More additions to Evaluate camera.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvaluateCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EvaluateCamera.java
@@ -17,20 +17,25 @@ import java.util.concurrent.TimeUnit;
 @TeleOp(name = "Camera Evaluation", group = "Test")
 public class EvaluateCamera extends OpMode {
     VisionPortal portal;
-    private long exposure;
-    private int minExposure;
-    private int maxExposure;
     private boolean isManualExposureSupported = false;
     private boolean isAutoExposureSupported = false;
     private boolean isAperturePriorityExposureSupported = false;
-    private boolean iaContinuousAutoExposureSuppported = false;
+    private boolean isContinuousAutoExposureSupported = false;
     private boolean isGainSupported = false;
-    private boolean isWhiteBalanceSupported = false;
+    private boolean isWhiteBalanceAutoSupported = false;
+    private boolean isWhiteBalanceManualSupported = false;
     private boolean isAutoFocusSupported = false;
     private boolean isFixedFocusSupported = false;
     private boolean isContinuousFocusSupported = false;
     private boolean isMacroFocusSupported = false;
     private boolean isInfinityFocusSupported = false;
+    private long exposure;
+    private int minExposure;
+    private int maxExposure;
+    private int minTemperature;
+    private int maxTemperature;
+    private int currentTemperature;
+    private int gain;
     private int minGain;
     private int maxGain;
     final int RESOLUTION_WIDTH = 640;
@@ -63,32 +68,38 @@ public class EvaluateCamera extends OpMode {
     private void ProcessCamera() {
         // Exposure
         ExposureControl exposureControl = portal.getCameraControl(ExposureControl.class);
-        if (exposureControl.isExposureSupported()) {
-            minExposure = (int) exposureControl.getMinExposure(TimeUnit.MILLISECONDS);
-            maxExposure = (int) exposureControl.getMaxExposure(TimeUnit.MILLISECONDS);
-            exposure = exposureControl.getExposure(TimeUnit.MILLISECONDS);
-        }
-
         isManualExposureSupported = exposureControl.isModeSupported(ExposureControl.Mode.Manual);
         isAutoExposureSupported = exposureControl.isModeSupported(ExposureControl.Mode.Auto);
         isAperturePriorityExposureSupported = exposureControl.isModeSupported(ExposureControl.Mode.AperturePriority);
-        iaContinuousAutoExposureSuppported = exposureControl.isModeSupported(ExposureControl.Mode.ContinuousAuto);
+        isContinuousAutoExposureSupported = exposureControl.isModeSupported(ExposureControl.Mode.ContinuousAuto);
+        // Set to manual so gain will work.
+        exposureControl.setMode(ExposureControl.Mode.Manual);
 
+        minExposure = (int) exposureControl.getMinExposure(TimeUnit.MILLISECONDS);
+        maxExposure = (int) exposureControl.getMaxExposure(TimeUnit.MILLISECONDS);
+        exposure = exposureControl.getExposure(TimeUnit.MILLISECONDS);
         // Gain
         GainControl gainControl = portal.getCameraControl(GainControl.class);
         minGain = gainControl.getMinGain();
         maxGain = gainControl.getMaxGain();
         isGainSupported = gainControl.setGain(5);
+        gain = gainControl.getGain();
 
         // White Balance
         WhiteBalanceControl whiteBalanceControl = portal.getCameraControl(WhiteBalanceControl.class);
-        isWhiteBalanceSupported = whiteBalanceControl.setMode(WhiteBalanceControl.Mode.AUTO);
+        isWhiteBalanceAutoSupported = whiteBalanceControl.setMode(WhiteBalanceControl.Mode.AUTO);
+        isWhiteBalanceManualSupported = whiteBalanceControl.setMode(WhiteBalanceControl.Mode.MANUAL);
+        whiteBalanceControl.setMode(WhiteBalanceControl.Mode.MANUAL);
+        whiteBalanceControl.setWhiteBalanceTemperature(6500);
+        minTemperature = whiteBalanceControl.getMinWhiteBalanceTemperature();
+        maxTemperature = whiteBalanceControl.getMaxWhiteBalanceTemperature();
+        currentTemperature = whiteBalanceControl.getMinWhiteBalanceTemperature();
 
         // Focus
         FocusControl focusControl = portal.getCameraControl(FocusControl.class);
         isAutoFocusSupported = focusControl.isModeSupported(FocusControl.Mode.Auto);
         isFixedFocusSupported = focusControl.isModeSupported(FocusControl.Mode.Fixed);
-        iaContinuousAutoExposureSuppported = focusControl.isModeSupported(FocusControl.Mode.ContinuousAuto);
+        isContinuousFocusSupported = focusControl.isModeSupported(FocusControl.Mode.ContinuousAuto);
         isMacroFocusSupported = focusControl.isModeSupported(FocusControl.Mode.Macro);
         isInfinityFocusSupported = focusControl.isModeSupported(FocusControl.Mode.Infinity);
 
@@ -100,7 +111,27 @@ public class EvaluateCamera extends OpMode {
         telemetry.addData("Manual Support", isManualExposureSupported);
         telemetry.addData("Auto Support", isAutoExposureSupported);
         telemetry.addData("Aperture Support", isAperturePriorityExposureSupported);
+        telemetry.addData("Continuous Support", isContinuousAutoExposureSupported);
+        telemetry.addData("Min", minExposure);
+        telemetry.addData("Max", maxExposure);
+        telemetry.addData("Current", exposure);
+        telemetry.addLine("Gain");
+        telemetry.addData("Gain Support", isGainSupported);
+        telemetry.addData("Min", minGain);
+        telemetry.addData("Max", maxGain);
+        telemetry.addData("Current", gain);
+        telemetry.addLine("White Balance");
+        telemetry.addData("Auto Support", isWhiteBalanceAutoSupported);
+        telemetry.addData("Manual Support", isWhiteBalanceManualSupported);
+        telemetry.addData("Min", minTemperature);
+        telemetry.addData("Max", maxTemperature);
+        telemetry.addData("Current", currentTemperature);
+        telemetry.addLine("Focus");
+        telemetry.addData("Auto Support", isAutoFocusSupported);
+        telemetry.addData("Fixed Support", isFixedFocusSupported);
+        telemetry.addData("Macro Support", isMacroFocusSupported);
         telemetry.addData("Continuous Support", isContinuousFocusSupported);
+        telemetry.addData("Infinity Support", isInfinityFocusSupported);
         telemetry.update();
     }
 }
