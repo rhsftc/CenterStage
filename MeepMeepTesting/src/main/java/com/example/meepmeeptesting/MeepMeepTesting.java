e40088e20da33b249bb84b3b8df4cfb56d0a55fb
rhsftc <ronsearle@hotmail.com>
2023-10-11 13:59:21 -0500

    Installed MeepMeep with centerstage field.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,12 +1,11 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class meepmeeptesting {
+public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -15,21 +14,18 @@ public class meepmeeptesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .splineTo(new Vector2d(10, 10), 0)
+                                .forward(30)
                                 .turn(Math.toRadians(90))
-                                .splineTo(new Vector2d(25, -15), 0)
-                                .waitSeconds(2)
-                                .turn(Math.toRadians(45))
-                                .forward(10)
-                                .strafeRight(5)
+                                .forward(30)
                                 .turn(Math.toRadians(90))
-                                .strafeLeft(5)
-                                .waitSeconds(1)
-                                .splineToLinearHeading(new Pose2d(-10, -10, Math.toRadians(45)), 0)
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
                                 .build()
                 );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
