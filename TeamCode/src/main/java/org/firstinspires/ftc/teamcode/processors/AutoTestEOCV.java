e3d939d26422f845b8f3cfdbd65b06f7b6fd462d
rhsftc <ronsearle@hotmail.com>
2023-11-06 11:54:00 -0600

    Set camera to auto white balance.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/AutoTestEOCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/processors/AutoTestEOCV.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.WhiteBalanceControl;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 @Autonomous(name = "Auto EOCV-SIM", group = "Camera")
@@ -22,13 +23,26 @@ public class AutoTestEOCV extends OpMode {
         visionPortalBuilder = new VisionPortal.Builder();
         visionPortal = visionPortalBuilder.enableLiveView(true).
                 addProcessor(imageProcessor).
-                setCamera(hardwareMap.get(WebcamName.class, "0")).
+                setCamera(hardwareMap.get(WebcamName.class, "webcam1")).
                 setCameraResolution(new Size(640, 480)).
+                setStreamFormat(VisionPortal.StreamFormat.MJPEG).
                 build();
     }
 
     @Override
     public void init_loop() {
+        // Wait for the camera to be open
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+            WhiteBalanceControl whiteBalanceControl = visionPortal.getCameraControl(WhiteBalanceControl.class);
+            whiteBalanceControl.setMode(WhiteBalanceControl.Mode.AUTO);
+        }
+
         telemetry.addData("Init Identified", imageProcessor.getSelection());
         telemetry.update();
     }
