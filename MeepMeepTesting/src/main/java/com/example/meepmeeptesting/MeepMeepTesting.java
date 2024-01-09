f545ea15acdccb0b872fc35af1bc7e57eb61d610
rhsftc <ronsearle@hotmail.com>
2024-01-09 10:39:59 -0600

    Gradle Update

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -8,17 +9,20 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(640);
+        MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -69, Math.toRadians(90)))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
-                .splineTo(new Vector2d(0, 60), Math.toRadians(-90))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -62, Math.toRadians(90)))
+                .splineTo(new Vector2d(-46, -32), Math.toRadians(90))
+                .stopAndAdd(new SleepAction(1.5))
+                .lineToY(-60)
+                .turn(Math.toRadians(-90))
+                .lineToX(12)
+                .splineTo(new Vector2d(40, -35), Math.toRadians(0))
 //                .lineToX(0)
 //                .turn(Math.toRadians(90))
 //                .lineToY(0)
