d6609b62866c6456cfba8c3c67b80f92c5aa5097
rhsftc <ronsearle@hotmail.com>
2024-01-11 15:32:52 -0600

    Meep meep paths for auto.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -17,16 +17,35 @@ public class MeepMeepTesting {
                 .build();
 
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-36, -62, Math.toRadians(90)))
-                .splineTo(new Vector2d(-46, -32), Math.toRadians(90))
-                .stopAndAdd(new SleepAction(1.5))
+                // Left
+                .splineTo(new Vector2d(-48, -32), Math.toRadians(90))
                 .lineToY(-60)
                 .turn(Math.toRadians(-90))
                 .lineToX(12)
-                .splineTo(new Vector2d(40, -35), Math.toRadians(0))
-//                .lineToX(0)
-//                .turn(Math.toRadians(90))
-//                .lineToY(0)
-//                .turn(Math.toRadians(90))
+                .splineTo(new Vector2d(50, -30), Math.toRadians(0))
+                .stopAndAdd(new SleepAction(1.5))
+                .splineTo(new Vector2d(-36, -62), Math.toRadians(90))
+                .stopAndAdd(new SleepAction(1.5))
+                // Middle
+                .lineToY(-32)
+                .lineToY(-60)
+                .turn(Math.toRadians(-90))
+                .lineToX(12)
+                .splineTo(new Vector2d(50, -36), Math.toRadians(0))
+                .stopAndAdd(new SleepAction(1.5))
+                .splineTo(new Vector2d(-36, -62), Math.toRadians(90))
+                // right
+                .stopAndAdd(new SleepAction(1.5))
+                .lineToY(-48)
+                .splineTo(new Vector2d(-24, -32), Math.toRadians(90))
+                .strafeTo(new Vector2d(-36, -32))
+                .setTangent(Math.toRadians(90))
+                .lineToY(-60)
+                .turn(Math.toRadians(-90))
+                .lineToX(12)
+                .splineTo(new Vector2d(50, -42), Math.toRadians(0))
+                .stopAndAdd(new SleepAction(1.5))
+                .splineTo(new Vector2d(-36, -62), Math.toRadians(90))
                 .build());
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
